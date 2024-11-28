9327b227b016c6e00d803ed09e19073d2d52281a
Harry Randazzo <harry@razzle.cloud>
2024-11-27 20:26:07 -0600

    working
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 <https://xkcd.com/936/>
 
+First goal is to replicate the following in a statically linked go binary:
+
+```bash
+grep "^[^']\{3,5\}$" /usr/share/dict/words | shuf -n4 | paste -sd'.' -
+```
 
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+
+	"github.com/noxsios/ezpass/words"
+)
 
 func main() {
-	fmt.Println("ezpass")
+	length := 4
+	separator := "."
+
+	max := big.NewInt(int64(len(words.ALL)))
+
+	for i := range length {
+		randBigInt, err := rand.Int(rand.Reader, max)
+		if err != nil {
+			panic(err)
+		}
+
+		index := int(randBigInt.Int64())
+
+		fmt.Print(words.ALL[index])
+		if i < length-1 {
+			fmt.Print(separator)
+		}
+	}
 }
