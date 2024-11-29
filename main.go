645053f03cd9ff288ed77079d247fc4ab46b9d1b
Harry Randazzo <harry@razzle.cloud>
2024-11-28 23:20:56 -0600

    simple version flag
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math/big"
 	"os"
+	"runtime/debug"
 	"slices"
 
 	"github.com/noxsios/ezpass/words"
@@ -13,18 +14,31 @@ import (
 
 func main() {
 	var n int
-	flag.IntVar(&n, "n", 4, "Number of words in resulting password")
+	flag.IntVar(&n, "n", 4, "Number of words in resulting password.")
 
 	var delimiter string
-	flag.StringVar(&delimiter, "d", ".", "Delimiter between words")
+	flag.StringVar(&delimiter, "d", ".", "Delimiter between words.")
 
 	var help bool
 	flag.BoolVar(&help, "h", false, "Print this message and exit.")
+
+	var ver bool
+	flag.BoolVar(&ver, "v", false, "Print the version number of ezpass and exit.")
 
 	flag.Parse()
 
 	if help || slices.Contains(os.Args[1:], "--help") {
 		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if ver {
+		bi, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Println("version information not available")
+			os.Exit(1)
+		}
+		fmt.Println(bi.Main.Version)
 		os.Exit(0)
 	}
 
