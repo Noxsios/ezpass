042853136f2157066d8c6f50f2a538bee7a78761
Harry Randazzo <harry@razzle.cloud>
2025-11-26 18:44:01 -0600

    fix lint
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- words/wordlist.go
+++ words/wordlist.go
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024-Present Harry Randazzo
+
 package words
 
 import (
@@ -6,15 +9,19 @@ import (
 	"github.com/spf13/pflag"
 )
 
+// Wordlist represents a wordlist type that can be used for password generation.
 type Wordlist string
 
 var _ pflag.Value = (*Wordlist)(nil)
 
 const (
-	EffWordlist          Wordlist = "eff"
+	// EffWordlist represents the EFF wordlist.
+	EffWordlist Wordlist = "eff"
+	// UsrShareDictWordlist represents the system dictionary wordlist.
 	UsrShareDictWordlist Wordlist = "usr_share_dict"
 )
 
+// AvailableWordlists returns a slice of all available wordlist names.
 func AvailableWordlists() []string {
 	return []string{
 		string(EffWordlist),
@@ -22,6 +29,7 @@ func AvailableWordlists() []string {
 	}
 }
 
+// Set implements the pflag.Value interface to allow setting the wordlist from command line flags.
 func (w *Wordlist) Set(s string) error {
 	switch s {
 	case string(EffWordlist):
@@ -41,6 +49,7 @@ func (w *Wordlist) String() string {
 	return string(*w)
 }
 
+// Type implements the pflag.Value interface to return the type name for help text.
 func (w *Wordlist) Type() string {
 	return "wordlist"
 }
